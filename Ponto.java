2082995d1f2d1a496d7195e491a68053d9542cc1
MariaGeovana <smaria.geovana@yahoo.com>
2017-02-23 11:07:04 -0300

    reta

--- Ponto.java
+++ Ponto.java
@@ -1,5 +1,13 @@
 public class Ponto{
-private	double x,y;
+	double x,y;
+public Ponto(){
+System.out.println("construtor 1");
+}
+public  Ponto(double x , double y){
+System.out.println("construtor 2");
+	this.x =x;
+	this.y =y;
+}
 public void setX (double x){
 	this.x = x;
 }
@@ -15,7 +23,8 @@ public double getY(){
 public static void  main (String [] args){
 	double a,b,c,d;
 	Ponto p1 = new Ponto();
-	Ponto p2 =new Ponto();	
+	Ponto p2 =new Ponto();
+	Ponto p4= new Ponto(4,0);
 	p1.setX(4);
 	p1.setY(5);
 	p1.getX();
@@ -29,5 +38,18 @@ public static void  main (String [] args){
  d= Math.pow(a,2)+Math.pow(b,2);
 c=Math.sqrt(d);
 System.out.println(c);
+ Reta r1= new Reta();
+        r1.setP1(p1);
+        r1.setP2(p2);
+        r1.getP1().getX();
+        r1.getP1().getY();
+        r1.getP2().getX();
+        r1.getP2().getY();
+        p1.setX(30);
+        r1.getP2().setY(40);
+
+        System.out.println(r1.getP1().getX()+" "+ r1.getP1().getY()+" "+r1.getP2().getX()+" "+r1.getP2().getY());       
+        p1.setX(4);
+
 }
 }
