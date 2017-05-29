cd016e6c31094ca83be78a9d7697d7d574ceff08
MariaGeovana <smaria.geovana@yahoo.com>
2017-05-29 12:34:18 -0300

    Exemplo

--- Trabalho1Android/app/src/main/java/com/example/maria/maria/MainActivity.java
+++ Trabalho1Android/app/src/main/java/com/example/maria/maria/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.maria.maria;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void salvar(View vi){
+        TextView txt1 = (TextView)findViewById(R.id.InputNome);
+        txt1.setText("Maria");
+        String valor1 = txt1.getText().toString();
+        TextView txt2 = (TextView)findViewById(R.id.InputTel);
+        txt2.setText(valor1);
+
+    }
+
 }
